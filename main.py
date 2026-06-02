1aef644ad67431577cb9291d7cba8797bd310eb5
fred <frednobre18@gmail.com>
2026-06-02 21:48:33 +0200

    test smtp

---
+++ main.py
@@ -0,0 +1,17 @@
+from smtp_service import SMTPService
+
+SMTP_SERVER = SMTP_PORT = EMAIL_USERNAME = EMAIL_PASSWORD = None
+with open('.env') as f:
+    for line in f:
+        if line.startswith('SMTP_SERVER'):
+            SMTP_SERVER = line.split('=')[1].strip()
+        elif line.startswith('SMTP_PORT'):
+            SMTP_PORT = int(line.split('=')[1].strip())
+        elif line.startswith('EMAIL_USERNAME'):
+            EMAIL_USERNAME = line.split('=')[1].strip()
+        elif line.startswith('EMAIL_PASSWORD'):
+            EMAIL_PASSWORD = line.split('=')[1].strip()
+
+smtp = SMTPService(SMTP_SERVER, SMTP_PORT, EMAIL_USERNAME, EMAIL_PASSWORD)
+print(SMTP_SERVER, SMTP_PORT, EMAIL_USERNAME, EMAIL_PASSWORD)
+smtp.send_email("frednobre18@gmail.com", "Test Email", "This is a test email from the SMTP service.")
